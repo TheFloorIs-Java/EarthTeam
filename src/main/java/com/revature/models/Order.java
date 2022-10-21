edf4dfbd9523d9ec48aa2b86c21c76a0464301ab
MattHill92 <matthew765@revature.net>
2022-10-21 11:23:22 -0700

    final order-history

--- src/main/java/com/revature/models/Order.java
+++ src/main/java/com/revature/models/Order.java
@@ -41,7 +41,6 @@ public class Order {
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private int id;
 
-
         @ManyToOne
         @JoinColumn(name = "product_id")
         private Product product;
